713305176f1ae3d8907a8ddbc0068df2c15aaff7
takeda shinji <takeda.s.ad@m.titech.ac.jp>
2019-06-13 18:33:43 +0900

    no message

--- MU-2_Arduino_model/MU-2_Arduino_model.ino
+++ MU-2_Arduino_model/MU-2_Arduino_model.ino
@@ -14,7 +14,7 @@ void setup(){
   Serial.println("http://www.circuitdesign.jp/jp/products/EOL/doc/MU2_429_v1.pdf");
   Serial.println("簡易送信コマンド一覧(全てのコマンドの最後に必ず';'をつけてください.';'をつけなければならないのはこのプログラムだけの話です.付け忘れたら';'だけ打てば直ります.)");
   Serial.println("ユーザ ID 設定\n@UI XXXX,XXXX\n\nグループ ID 設定\n@GI XX\n\n機器 ID 設定\n@EI XX\n\n目的局 ID 指定\n@DI XX\n\n使用チャンネル設定\n@CH XX\n\nデータ送信コマンド\n@DT+'データサイズ'+'データ'\n(例)@DT09AMANOGAWA;(送信後に*DT=09がレスポンスされる) 受信機側には*DR=09AMANOGAWAと受信されます.\n最後に'/A'をつけると相手から受信確認通知として送信機が'*DR=00'を受信します.\n");
-  Serial.println("==重要==");
+  Serial.println("===重要===");
   Serial.println("TXはD9ピン, RXはD8ピンになっています.\n");
 }
  
